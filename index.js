f7cdfc3c3f41a50827b58692242da54470ce33bf
Valeriy Grishko <grishko28@gmail.com>
2020-04-29 18:23:47 +0300

    'monogDB'

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
+const mongoose = require('mongoose')
+const User = require('./models/user')
 
 const app = express()
 
@@ -17,6 +19,18 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5ea9983795454e27f0f7d4a7')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e);
+  }
+
+})
+
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
@@ -27,6 +41,31 @@ app.use('/card', cardRoutes)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+
+async function start() {
+  try {
+    const url = 'mongodb+srv://Valeriy:9tNZGkhyysS6Use@node-js-rgwad.mongodb.net/shop';
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    const candidate = await User.findOne()
+    if(!candidate){
+      const user = new User({
+        name: 'Valeriy',
+        email: 'grishko0806@gmail.com',
+        cart: {items: []}
+      })
+      await user.save()
+    }
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+     console.log(e)
+  }
+}
+
+start()
+
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [
+            {
+                count: {
+                    type: Number,
+                    required: true,
+                    default: 1
+                },
+                courseId: {
+                    required: true,
+                    ref: 'Course',
+                    type: Schema.Types.ObjectId
+                }
+            }
+        ]
+    }
+})
+
+module.exports = model('User', userSchema)
